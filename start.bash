f3d5d3834c7d3a54b6d6718eb5a9816c9b6f9a9a
Christian Stewart <kidovate@gmail.com>
2014-05-25 15:50:40 -0400

    Updates

--- start.bash
+++ start.bash
@@ -4,4 +4,4 @@ tmux new-session -d -s server
 #tmux new-window -t server:1 -n bash
 tmux send-keys -t server:0 "bash" C-m
 sleep 1s
-tmux send-keys -t server:0 "cd /root/d2mp/ && ROOT_URL=http://d2modd.in/ /usr/local/bin/meteor --production --port 80 --settings private/settings.json" C-m
+tmux send-keys -t server:0 "cd /root/d2mp/ && ROOT_URL=http://d2modd.in/ DDP_DEFAULT_CONNECTION_URL=http://ddp.d2modd.in /usr/local/bin/meteor --production --port 80 --settings private/settings.json" C-m
